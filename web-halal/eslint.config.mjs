68ea27816f1dd17b544d42832e08652fc2782720
duongnb <nguyenbinhduong26@gmail.com>
2026-07-01 21:58:54 +0700

    Refactor: sửa logo

--- web-halal/eslint.config.mjs
+++ web-halal/eslint.config.mjs
@@ -15,8 +15,7 @@ const eslintConfig = [
       "@typescript-eslint/ban-ts-comment": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "prefer-const": "off",
-      "react-hooks/exhaustive-deps": "error",
-      ""
+      "react-hooks/exhaustive-deps": "error"
     },
   }),
 ];
